ca55257ad58fe65881d84ae79836ef96712264ed
SubhasreeCodes <subhasreeps2001@gmail.com>
2025-01-28 19:47:45 +0530

    Logic to add default "no_image_available".

--- backend/admin.py
+++ backend/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from backend.models import Category, Brand
 
 # Register your models here.
 admin.site.register(Category)
-admin.site.register(Brand)
+# admin.site.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('id','name', 'image_tag')
+
+    search_fields = ('id','name')
+
+    def image_tag(self, obj):
+        if obj.image_path:
+            return format_html('<img src="{}" width="150" height="150" />'.format(obj.image_path.url))
+        return "No Image"  # In case there's no image
+
+    image_tag.short_description = 'Image'
+
+admin.site.register(Brand,BrandAdmin)
